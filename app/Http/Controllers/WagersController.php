bed6570325f7687603ffbaa37b94c223bf1a98de
namvo1988 <namvo1988@gmail.com>
2020-03-02 23:56:49 +0700

    First Commit

---
+++ app/Http/Controllers/WagersController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Http\Resources\ProductCollection as ProductCollection;
+use App\Http\Resources\ProductResource as ProductResource;
+
+use Illuminate\Http\Request;
+
+class WagersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return new ProductCollection(Product::all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, Product $product)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\Product  $product
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(Product $product)
+    // {
+    //     //
+    // }
+}
